8f8e9875de46ec849c8182676ed18fe360e03783
Dev Singh <sigmaplus128@gmail.com>
2025-07-28 23:56:33 +0530

    Add files via upload
    settimeout/interval/arrow function

---
+++ app.js
@@ -0,0 +1,41 @@
+
+/*let fun = (n) =>{
+    return n*n;
+}
+
+let a = setInterval( () => {
+    console.log("Hello world");
+    console.log("Hello world");
+    console.log("Hello world");
+    console.log("Hello world");
+    console.log("Hello world");
+}, 2000);
+
+ 
+
+
+//1
+let arr = [1,2,3]
+let Arrayaverage = (arr) =>{
+    let avg = 0;
+    for(let i=0;i<arr.length;i++){
+        avg = avg + arr[i];
+    }
+    return avg/arr.length;
+};
+
+console.log(Arrayaverage(arr));
+
+let arr = [1,2,3]
+let Arrayaverage = (arr) =>{
+    let avg = 0;
+    for(let i=0;i<arr.length;i++){
+        avg = avg + arr[i];
+    }
+    return avg/arr.length;
+};
+
+console.log(Arrayaverage);
+*/
+
+//2
