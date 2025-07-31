840ac44b1d29bf5de54bef86ad58b35edfec4902
Dev Singh <sigmaplus128@gmail.com>
2025-07-31 23:04:47 +0530

    Add files via upload
    Day 1 (sheet 1)

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
-
-/*let fun = (n) =>{
-    return n*n;
-}
-
-let a = setInterval( () => {
-    console.log("Hello world");
-    console.log("Hello world");
-    console.log("Hello world");
-    console.log("Hello world");
-    console.log("Hello world");
-}, 2000);
-
- 
-
-
-//1
-let arr = [1,2,3]
-let Arrayaverage = (arr) =>{
-    let avg = 0;
+//Square and sum the array elements using the arrow function and then find the average of the array
+let arr = [1,2,3,4];
+let ans = (sum,arr) => {
     for(let i=0;i<arr.length;i++){
-        avg = avg + arr[i];
+        sum += arr[i]*arr[i]
     }
-    return avg/arr.length;
+    return sum;
 };
 
-console.log(Arrayaverage(arr));
+console.log(ans(0,arr));
 
-let arr = [1,2,3]
-let Arrayaverage = (arr) =>{
-    let avg = 0;
-    for(let i=0;i<arr.length;i++){
-        avg = avg + arr[i];
-    }
-    return avg/arr.length;
-};
 
-console.log(Arrayaverage);
-*/
+// Create a new array using the map function whose each element is equal to the original element plus 5.
+let newarr = arr.map((el) => el+5);
+console.log(newarr);
 
-//2
+/*Write a function called doubleAndReturnArgs which accepts an array and a
+ variable number of arguments. The function should return a new array with the original
+ array values and all of the additional arguments doubled*/
+
+ let doubleAndReturnArgs  = (arr, ...args) => [
+    ...arr,
+    ...args.map((v)=>v*2),
+ ]
+
+ console.log(doubleAndReturnArgs([1,2,3,4],4,4));
+
+//merge two objects
+let mergeobj = (obj1,obj2) => ({...obj1,...obj2});
+console.log(mergeobj({k:1 , l:2},{m:3 , n:4}));
